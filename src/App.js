5f678ab9edd1c72c3f1553a7dd360b2b47e5bd89
Anastasiia <anasta_ok@mail.ru>
2022-08-11 17:23:44 +0300

    added PizzasBlock, main props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const API = "http://localhost:3000/db.json";
+
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home items={pizzas} />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
