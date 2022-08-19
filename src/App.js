b6339c3c807c67ac0929f257d08e0865608b2457
Anastasiia <anasta_ok@mail.ru>
2022-08-19 16:27:06 +0300

    update redux react:added useSelector, useDispatch

--- src/App.js
+++ src/App.js
@@ -1,57 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Routes, Route } from "react-router-dom";
 import { setPizzas } from "./redux/actions/pizzas";
 
-// function App() {
-//   const API = "http://localhost:3000/db.json";
+function App() {
+  const dispatch = useDispatch();
 
-//   const [pizzas, setPizzas] = useState([]);
+  const { items } = useSelector(({ pizzas, filter }) => {
+    return {
+      items: pizzas.items,
+      sortBy: filter.sortBy,
+    };
+  });
 
-//   useEffect(() => {
-//     axios.get(API).then(({ data }) => {
-//       setPizzas(data.pizzas);
-//     });
-//   }, []);
-
-//   return ;
-// }
-
-class App extends React.Component {
-  componentDidMount() {
+  useEffect(() => {
     axios.get("http://localhost:3000/db.json").then(({ data }) => {
-      this.props.setPizzas(data.pizzas);
+      dispatch(setPizzas(data.pizzas));
     });
-  }
+  }, []);
 
-  render() {
-    console.log(this.props.items);
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home items={this.props.items} />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </div>
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home items={items} />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-  };
-};
-
-const mapDispatchToProps = {
-  setPizzas,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,9 +19,7 @@ const Home = ({ items }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {items.map((obj) => (
-          <PizzasBlock key={obj.id} {...obj} />
-        ))}
+        {items && items.map((obj) => <PizzasBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   );
