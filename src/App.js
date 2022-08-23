6a70af9d003686d3566a66fe8ed47789d893d863
Anastasiia <anasta_ok@mail.ru>
2022-08-23 12:36:39 +0300

    added React-thunk

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import { useDispatch } from "react-redux";
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Routes, Route } from "react-router-dom";
-import { setPizzas } from "./redux/actions/pizzas";
+import { fetchPizzas, setPizzas } from "./redux/actions/pizzas";
 
 function App() {
   const dispatch = useDispatch();
 
+  // установка JSON через global в powershell
+  // C:\Users\79375\desktop\Учеба\react\react-pizza> json-server --watch public/db.json --port 3001
+
   useEffect(() => {
-    axios.get("http://localhost:3001/pizzas").then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
+    console.log(dispatch(fetchPizzas()));
   }, []);
 
   return (
