2bedef9139e7a2a972f5cf4f2d72c39d75e17aee
Anastasiia <anasta_ok@mail.ru>
2022-08-30 16:35:03 +0300

    added CartItem

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Categories } from "./Categories";
 export { default as SortPopup } from "./SortPopup";
 export { default as PizzasBlock } from "./PizzaBlock";
 export { default as PizzaLoadingBlock } from "./PizzaBlock/LoadingBlock";
+export { default as CartItems } from "./CartItem";
