84138be995481a0ded90bc6fcbe51af25a87b24a
Anastasiia <anasta_ok@mail.ru>
2022-09-01 12:56:39 +0300

    update pizzas action

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -6,7 +6,11 @@ export const setLoaded = (payload) => ({
 });
 
 export const fetchPizzas = (sortBy, category) => (dispatch) => {
-  dispatch(setLoaded(false));
+  dispatch({
+    type: "SET_LOADED",
+    payload: false,
+  });
+
   axios
     .get(
       `/pizzas?${category !== null ? `category=${category}` : ""}&_sort=${
